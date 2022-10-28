e58445fc24eb43db5908d1f42fdb4eb2c450c4ea
Bean <bddang@svsu.edu>
2022-10-28 11:00:46 -0400

    put to folder

--- CIS422_Capstone_GUI/CIS422_Capstone_GUI/Main_menu.vb
+++
@@ -1,5 +0,0 @@
-﻿Public Class Main_menu
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-End Class
--- CIS422_Capstone_GUI/CIS422_Capstone_GUI/filter_options.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class filter_options
-    Private Sub Label5_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Label4_Click(sender As Object, e As EventArgs)
-
-    End Sub
-End Class
