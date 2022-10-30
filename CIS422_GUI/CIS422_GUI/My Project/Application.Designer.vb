3548deaa415bf579085fab11fee7a61a720fd76b
Bean <bddang@svsu.edu>
2022-10-30 17:30:45 -0400

    Main menu

--- CIS422_GUI/CIS422_GUI/My Project/Application.Designer.vb
+++ CIS422_GUI/CIS422_GUI/My Project/Application.Designer.vb
@@ -32,8 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.CIS422_GUI.MainScreen
-
+            Me.MainForm = Global.CIS422_GUI.admin
         End Sub
     End Class
 End Namespace
