d195dd2c2ca6006cb766b1811cc474ffc1f41dd3
Bean <bddang@svsu.edu>
2022-10-29 21:21:29 -0400

    add main screen

--- CIS422_GUI/CIS422_GUI/View Schedule Screen/MainScreen.vb
+++ CIS422_GUI/CIS422_GUI/View Schedule Screen/MainScreen.vb
@@ -2,4 +2,12 @@
     Private Sub SplitContainer1_Panel1_Paint(sender As Object, e As PaintEventArgs)
 
     End Sub
+
+    Private Sub TabPage3_Click(sender As Object, e As EventArgs) Handles TabPage3.Click
+
+    End Sub
+
+    Private Sub MainScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
